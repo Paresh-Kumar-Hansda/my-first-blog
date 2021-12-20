59fa623b00ea7ee01bca09cf23c3dec6c4d53e65
paresh <mystylepkh@gmail.com>
2021-12-20 15:17:10 +0530

    in Setting.py Secret key disabled

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l8sxrc(ie7t4c=ut70*e_up)te9f1tp)kk2bp(iignq6wqt$zx'
+#SECRET_KEY = 'django-insecure-l8sxrc(ie7t4c=ut70*e_up)te9f1tp)kk2bp(iignq6wqt$zx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
